b116292d87c988bbf83467ed14159e3d5614795b
itishn1k <kayrat1224mamataev@gmail.com>
2023-04-02 04:37:51 +0600

    Revert "init project"
    This reverts commit de546639b39106ec67da12a327078baf01326dba.

--- app/src/main/java/com/example/a7app/data/local/mapper/NoteMappers.kt
+++
@@ -1,8 +0,0 @@
-package com.example.a7app.data.local.mapper
-
-import com.example.a7app.data.model.NoteEntity
-import com.example.a7app.domain.model.Note
-
-fun Note.toNoteEntity() = NoteEntity(id, title, description)
-
-fun NoteEntity.toNote() = Note(id, title, description)
